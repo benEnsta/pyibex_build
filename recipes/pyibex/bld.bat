cb9aed9ddda333c0166b82e2c4abe36491c70b54
Benoit DESROCHERS <ben.ensta@gmail.com>
2016-08-10 14:48:54 +0200

    [master] debug script

--- recipes/pyibex/bld.bat
+++ recipes/pyibex/bld.bat
@@ -1,4 +1,11 @@
-"%PYTHON%" setup.py build_ext -v -I "%CONDA_PREFIX%"/include:"%CONDA_PREFIX%"/include/ibex -L "%CONDA_PREFIX%"/lib
+ls "%CONDA_PREFIX%"\include\ibex
+SET DISTUTILS_USE_SDK=1
+SET MSSdk=1
+"%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:15
+"%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x64 /MT /release
+
+"%PYTHON%" setup.py build_ext -I "%CONDA_PREFIX%"\include\ibex -L "%CONDA_PREFIX%"\lib -DWIN32 -D/MT -D/Release
+if errorlevel 1 exit 1
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
 
--- recipes/pyibex/build.sh
+++ recipes/pyibex/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# $PYTHON setup.py build_ext -v -I ${CONDA_PREFIX}/include:${CONDA_PREFIX}/include/ibex -L ${CONDA_PREFIX}/lib
-$PYTHON setup.py install -v
+$PYTHON setup.py build_ext -I ${CONDA_PREFIX}/include:${CONDA_PREFIX}/include/ibex -L ${CONDA_PREFIX}/lib
+
+$PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
 
